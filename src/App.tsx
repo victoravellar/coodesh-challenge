e6a39142581d17f17054d5a890b72f4cbbfb884a
victoravellar <victor_avellar_97@hotmail.com>
2022-08-03 16:03:09 -0300

    chore: adding styled components lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+import HttpService from "./remote/httpService";
 
 function App() {
+  useEffect(() => {
+    HttpService.getWord("hello");
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
