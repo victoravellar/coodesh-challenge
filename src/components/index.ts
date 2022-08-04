f32129abf78fdd15ab6cafea3524fa843aa94d74
victoravellar <victor_avellar_97@hotmail.com>
2022-08-04 13:21:51 -0300

    chore: package.json

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as FavoritesWords } from "./FavoritesWords/FavoritesWords";
+export { default as WordBox } from "./WordBox/WordBox";
+export { default as WordsList } from "./WordsList/WordsList";
+export { default as WordsView } from "./WordsView/WordsView";
+export { default as WordsHeader } from "./WordsHeader/WordsHeader";
--- src/remote/httpService.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default abstract class HttpService {
-  static getWord(word: string) {
-    return axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-  }
-}
