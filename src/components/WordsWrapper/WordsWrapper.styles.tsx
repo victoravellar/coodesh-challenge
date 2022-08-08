c736171e6b3f138a17abf341e542bf3620d4b5d8
victoravellar <victor_avellar_97@hotmail.com>
2022-08-08 19:15:28 -0300

    refactor: words table component

--- src/components/WordsWrapper/WordsWrapper.styles.tsx
+++ src/components/WordsWrapper/WordsWrapper.styles.tsx
@@ -3,17 +3,21 @@ import styled from "styled-components";
 export const WordsTable = styled.table`
   display: flex;
   flex-wrap: wrap;
-  border: 3px solid black;
   max-height: 200px;
+  padding: 0.4rem;
+  background-color: #f1f1f1;
+  border-radius: 8px;
   overflow-y: auto;
   border-radius: 10px;
+  background-color: #f1f1f2;
 `;
 
 export const TdElement = styled.td`
-  border: 3px solid black;
   margin: 0.4rem;
-  padding: 1rem;
+  padding: 0.5rem 1rem;
   font-size: 1.4rem;
+  background-color: orange;
+  border-radius: 50px;
   display: flex;
   flex-direction: column;
   cursor: pointer;
